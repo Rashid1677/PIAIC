30f47fb87792307418144d6d034755d384dbc8ee
Rashid Munir <rashidmunir804@gmail.com>
2023-07-30 00:05:46 +0500

    first

---
+++ Assaignment1.ts
@@ -0,0 +1,30 @@
+console.log("HEY ADU MUNIR HUSSAIN,I HAVE STARTED LEARNING TYPESCRIPY");
+var myname:string="Rashid munir"
+console.log("My name is >=",myname);
+var num1:number = 1
+var num2:number = 2
+var num3:number = 3
+var num4:number = 4
+var num5:number = 5
+var num6:number = 6
+var num7:number = 7
+var num8:number = 8
+var num9:number = 9
+var num10:number = 10
+var sum = num1+num2+num3+num4+num5+num6+num7+num8+num9+num10
+console.log("Sum is>=",sum);
+var sub:number = num1-num2-num3-num4-num5-num6-num7-num8-num9-num10
+console.log("Subtraction is >=",sub);
+var munti:number = num1*num2*num3*num4*num5*num6*num7*num8*num9*num10
+console.log("Multiplication is >=",munti);
+var division:number = num10/num5
+console.log("Division is >=",division);
+var allop:number = num10/num5*num4+num2-num3
+console.log("All operation is >=",allop);
+
+
+
+
+
+
+
